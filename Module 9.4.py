a1051316bbfb9f09feee8dcfe7c555c4b153753f
MaksimOvsy <Chiefengserv@icloud.com>
2024-08-10 14:04:59 +0300

    Module 9.4.py
    Внесены изменения в запись текстового файла.

--- Module 9.4.py
+++ Module 9.4.py
@@ -23,7 +23,8 @@ print(result2)
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
         with open(file_name, 'a', encoding='utf-8') as file:
-            file.write(f'{data_set}')
+            for value in data_set:
+                file.write(f'{value}\n')
 
     return write_everything
 
