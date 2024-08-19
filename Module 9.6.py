b4b96fadb1c128c85fb17981925e4de80ba3bb96
MaksimOvsy <Chiefengserv@icloud.com>
2024-08-19 17:57:55 +0300

    Module 9.6.py

--- Module 9.6.py
+++ Module 9.6.py
@@ -22,22 +22,10 @@ abc
 
 
 def all_variants(text):
-    for i in range(len(text)):
-        m = text[i]
-        yield m
-    if len(m) < len(text):
-        for j in range(len(text) - 1):
-            m = text[j]
-            b = text[j+1]
-            if m != b:
-                yield m+b
-    if len(m+b) < len(text):
-        for k in range(len(text)-2):
-            m = text[k]
-            b = text[k + 1]
-            f = text[k + 2]
-            if f not in (m+b):
-                yield m+b+f
+    x = len(text) + 1
+    for i in range(1, x):
+        for j in range(x - i):
+            yield text[j: j+i]
 
 a = all_variants("abc")
 for i in a:
