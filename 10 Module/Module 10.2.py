e91e3e6d3d42cc5eb0c7c531f73165f459bf1ac5
MaksimOvsy <Chiefengserv@icloud.com>
2024-08-27 15:29:22 +0300

    Module 10.2.py
    "Потоки на классах"

--- 10 Module/Module 10.2.py
+++ 10 Module/Module 10.2.py
@@ -16,14 +16,12 @@ class Knight(Thread):
         while True:
             time.sleep(1)
             count += 1
-            m = enemy - self.power
-            if m >= 0:
-                print(f"{self.name} сражается {count} дня(-ей), осталось {m} воинов.")
-                yield enemy
+            enemy -= self.power
+            if enemy >= 0:
+                print(f"{self.name} сражается {count} дня(-ей), осталось {enemy} воинов.")
             else:
                 print(f"{self.name} одержал победу спустя {count} дня(-ей)!!")
                 break
-
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
