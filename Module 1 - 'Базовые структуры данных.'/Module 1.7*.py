0a71b7b33371e9dbf1975ba57b7883c82abce94b
MaksimOvsy <Chiefengserv@icloud.com>
2024-09-17 13:20:33 +0300

    Module 1.7*.py
    Дополнительное практическое задание по модулю: "Базовые структуры данных."

--- Module 1 - 'Базовые структуры данных.'/Module 1.7*.py
+++ Module 1 - 'Базовые структуры данных.'/Module 1.7*.py
@@ -26,9 +26,3 @@ students = list(sorted(students))
 unit_ = dict()
 unit_.update({students[0]:(sum(grades[0])/len(grades[0])),students[1]:(sum(grades[1])/len(grades[1])),students[2]:(sum(grades[2])/len(grades[2])),students[3]:(sum(grades[3])/len(grades[3])),students[4]:(sum(grades[4])/len(grades[4]))})
 print("Средний балл: ", unit_)
-
-
-
-
-
-
