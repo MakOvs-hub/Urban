6634e5788527d52d4578b241feba78e29bb031fc
MaksimOvsy <Chiefengserv@icloud.com>
2024-09-17 13:17:23 +0300

    Module 1.5.py
    "Неизменяемые и изменяемые объекты. Кортежи"

--- Module 1/Module 1.5.py
+++
@@ -1,9 +0,0 @@
-immutable_var = 1, 2, "a", "b", True
-print(immutable_var)
-#immutable_var[0]= 112
-#print(immutable_var) - данная команда исполняться не будет ввиду неизмености значения переменных кортежа (tuple).
-
-Mutable_list = [1, 2, "a", "b", True]
-print(Mutable_list)
-Mutable_list[0]= "Вода"
-print(Mutable_list)
