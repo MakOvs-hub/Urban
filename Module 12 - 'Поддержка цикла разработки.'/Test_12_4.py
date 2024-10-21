cd6253be8e8e0c5fae997044532c20548c29f188
MaksimOvsy <Chiefengserv@icloud.com>
2024-10-21 18:34:26 +0300

    Test_12_4.py
    "Логирование"

--- Module 12 - 'Поддержка цикла разработки.'/Test_12_4.py
+++ Module 12 - 'Поддержка цикла разработки.'/Test_12_4.py
@@ -23,141 +23,38 @@ test_run:
 В блоке except обработайте исключение соответствующего типа и логируйте его на уровне WARNING с сообщением "Неверный тип данных для объекта Runner".
 
 '''
-
-import logging
-import unittest
-
-logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='UTF-8', format='%(asctime)s | %(levelno)s | %(levelname)s | %(message)s')
-logging.critical('5')
-
-class Runner:
-    def __init__(self, name, speed=5):
-        if isinstance(name, str):
-            self.name = name
-        else:
-            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
-        self.distance = 0
-        if speed > 0:
-            self.speed = speed
-        else:
-            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
-
-    def run(self):
-        self.distance += self.speed * 2
-
-    def walk(self):
-        self.distance += self.speed
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return self.name
-
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return self.name == other
-        elif isinstance(other, Runner):
-            return self.name == other.name
-
-
-class Tournament:
-    def __init__(self, distance, *participants):
-        self.full_distance = distance
-        self.participants = list(participants)
-
-    def start(self):
-        finishers = {}
-        place = 1
-        while self.participants:
-            for participant in self.participants:
-                participant.run()
-                if participant.distance >= self.full_distance:
-                    finishers[place] = participant
-                    place += 1
-                    self.participants.remove(participant)
-
-        return finishers
-
-
-
-class RunnerTest(unittest.TestCase):
-    is_frozen = True
-
-    @unittest.skipIf(is_frozen == False, "Тесты в этом кейсе заморожены")
-    def test_walk(self):
-        r1 = Runner(name='Alex')
-        for _ in range(5):
-            r1.walk()
-        self.assertEqual(r1.distance, 25)
-        print('logging.info("walk")')
-
-    @unittest.skipIf(is_frozen == False, "Тесты в этом кейсе заморожены")
-    def test_run(self):
-        r2 = Runner(name='John')
-        for _ in range(10):
-            r2.run()
-        self.assertEqual(r2.distance, 100)
-        print('logging.info("run")')
-
-
-    @unittest.skipIf(is_frozen == False, "Тесты в этом кейсе заморожены")
-    def test_challenge(self):
-        r3 = Runner(name='Emma')
-        r4 = Runner(name='Olivia')
-        for _ in range(10):
-            r3.walk()
-            r4.run()
-        self.assertNotEqual(r3.distance, r4.distance)
-        print('logging.info("tournament")')
-
-
-if __name__ == '__main__':
-    print('Абра-Кадабра')
-    unittest.main()
-    # logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log',
-    #                     encoding='UTF-8', format='%(asctime)s | %(levelno)s | %(levelname)s | %(message)s')
-    # logging.critical('5')
-
-first = Runner('Вося', 10)
-second = Runner('Илья', 5)
-# # third = Runner('Арсен', 10)
-#
-t = Tournament(101, first, second)
-print(t.start())
-
-
-
-
-#Вариант с импортом файла.
-
-
-
 import rt_with_exceptions as rt
 import logging
 import unittest
 
-# logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='UTF-8', format='%(asctime)s | %(levelno)s | %(levelname)s | %(message)s')
-# logging.critical('5')
+logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log',
+                        encoding='UTF-8', format='%(asctime)s | %(levelno)s | %(levelname)s | %(message)s')
 
 class RunnerTest(unittest.TestCase):
     is_frozen = True
 
     @unittest.skipIf(is_frozen == False, "Тесты в этом кейсе заморожены")
     def test_walk(self):
-        r1 = rt.Runner(name='Alex')
-        for _ in range(5):
-            r1.walk()
-        self.assertEqual(r1.distance, 25)
-        print('logging.info("walk")')
+        try:
+            r1 = rt.Runner('Alex', speed=-5)
+            for _ in range(5):
+                r1.walk()
+            self.assertEqual(r1.distance, 25)
+            logging.info('"test_walk" выполнен успешно')
+        except:
+            logging.warning("Неверная скорость для Runner")
+
 
     @unittest.skipIf(is_frozen == False, "Тесты в этом кейсе заморожены")
     def test_run(self):
-        r2 = rt.Runner(name='John')
-        for _ in range(10):
-            r2.run()
-        self.assertEqual(r2.distance, 100)
-        print('logging.info("run")')
+        try:
+            r2 = rt.Runner(name=3654351)
+            for _ in range(10):
+                r2.run()
+            self.assertEqual(r2.distance, 100)
+            logging.info('"test_run" выполнен успешно')
+        except:
+            logging.warning("Неверный тип данных для объекта Runner")
 
 
     @unittest.skipIf(is_frozen == False, "Тесты в этом кейсе заморожены")
@@ -168,12 +65,4 @@ class RunnerTest(unittest.TestCase):
             r3.walk()
             r4.run()
         self.assertNotEqual(r3.distance, r4.distance)
-        print('logging.info("tournament")')
-
-
-if __name__ == '__main__':
-    print('Абра-Кадабра')
-    unittest.main()
-    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log',
-                        encoding='UTF-8', format='%(asctime)s | %(levelno)s | %(levelname)s | %(message)s')
-    logging.critical('5')
+        logging.info("tournament")
