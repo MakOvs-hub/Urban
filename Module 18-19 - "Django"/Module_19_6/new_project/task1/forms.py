05a4b04c9f82a6dc8adb2434dc06da03b4409e6c
MakOvs-hub <mr.freeman.75@mail.ru>
2024-12-20 15:23:51 +0300

    postgre

--- Module 18-19 - "Django"/Module_19_6/new_project/task1/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class UserRegister(forms.Form):
-    username = forms.CharField(max_length=30, label="Введите логин")
-    password = forms.CharField(min_length=8, label="Введите пароль")
-    repeat_password = forms.CharField(min_length=8, label="Повторите пароль")
-    age = forms.IntegerField(min_value=18, max_value=130, label="Введите свой возраст")
-
-
